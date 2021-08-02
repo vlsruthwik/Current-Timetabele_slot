db6ff6a285e172042d14001b0e0f1918e90328ca
vlsruthwik <71954352+vlsruthwik@users.noreply.github.com>
2021-08-03 05:19:40 +0530

    Add files via upload

--- Class update.py
+++ Class update.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import pyttsx3 as ts
 from datetime import datetime
-import threading
-import os
-import time
-def start_teams():
-    os.system('echo "%localappdata%/Microsoft/Teams/Update.exe" --processStart "Teams.exe">teamsrunner.bat')
-    time.sleep(1)
-    os.system("teamsrunner")
-    
-start_teams()
+import webbrowser
+
+
 cur_time = datetime.now().strftime("%I:00 %p")
 cur_day = datetime.now().strftime("%A")
+
+print('Today: ',cur_day)
 
 eng = ts.init()
 eng.setProperty('rate',125)
@@ -27,17 +23,26 @@ def timespeak(s):
 tt = pd.read_csv('S:/Class time.csv')
 tt = tt.set_index('week')
 tt = tt.fillna('No')
+times = ['09:00 AM','10:00 AM', '11:00 AM', '12:00 PM', '02:00 PM', '03:00 PM','04:00 PM']
 
-
-if(cur_day=='Sunday' or cur_time not in ['09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '02:00 PM', '03:00 PM','04:00 PM']):
+print()
+if cur_day!='Sunday':
+    today_sub = list(tt.loc[cur_day])
+    for i in range(7):
+        print("{} : {}".format(times[i],today_sub[i]))
+if(cur_day=='Sunday' or cur_time not in times):
     tell('Currently you are having No Class')
     
 else:
+    sub = tt.loc[cur_day][cur_time]
     next_i = list(tt.loc[cur_day].index).index(cur_time)+1
-    tell('Currently you are having'+tt.loc[cur_day][cur_time]+'Class')
+    tell('Currently you are having'+sub+'Class')
     
     if(next_i<7):
         tell('You are having'+tt.loc[cur_day][next_i]+'Class at'+timespeak(list(tt.loc[cur_day].index)[next_i]))
-    
-print(tt)
+
+print("\nPress Enter to Exit")
+input()
+
+
 
